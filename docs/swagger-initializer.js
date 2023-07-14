b9d5273d70461972cad2ec91abab6ad476f40147
jon-vdb <jonvdberg8@gmail.com>
2023-07-14 14:39:05 +0700

    OG styling

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -1,5 +1,3 @@
-import { SwaggerUIKongTheme } from 'swagger-ui-kong-theme'
-
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
@@ -7,30 +5,14 @@ window.onload = function() {
     url: "https://jonathan-vandenberg.github.io/api/swagger.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
-    filter: true,
     presets: [
       SwaggerUIBundle.presets.apis,
       SwaggerUIStandalonePreset
     ],
     plugins: [
-      SwaggerUIKongTheme,
       SwaggerUIBundle.plugins.DownloadUrl
     ],
-    layout: 'KongLayout',
-    theme:
-        {
-          "swaggerAbsoluteTop": "0px", // the top most container is set absolute at this distance from top. (default 0)
-          "hasSidebar": true, // enables sidebar (default off)
-          "languages" : [ // sets langagues for sidebar (default bash, javascript, python, ruby)
-            {
-              "prismLanguage":"bash",
-              "target":"shell",
-              "client":"curl"
-            },{
-              "prismLanguage":"ruby",
-              "target":"ruby"
-            }]
-        }
+    layout: "StandaloneLayout"
   });
 
   //</editor-fold>
