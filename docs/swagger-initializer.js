453bf951b752f2ba150fc2ffe216ba2e1f37b575
jon-vdb <jonvdberg8@gmail.com>
2023-07-14 13:26:13 +0700

    added swagger-ui-kong-theme

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -5,14 +5,30 @@ window.onload = function() {
     url: "https://jonathan-vandenberg.github.io/api/swagger.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
+    filter: true,
     presets: [
       SwaggerUIBundle.presets.apis,
       SwaggerUIStandalonePreset
     ],
     plugins: [
+      SwaggerUIKongTheme,
       SwaggerUIBundle.plugins.DownloadUrl
     ],
-    layout: "StandaloneLayout"
+    layout: 'KongLayout',
+    theme:
+        {
+          "swaggerAbsoluteTop": "0px", // the top most container is set absolute at this distance from top. (default 0)
+          "hasSidebar": true, // enables sidebar (default off)
+          "languages" : [ // sets langagues for sidebar (default bash, javascript, python, ruby)
+            {
+              "prismLanguage":"bash",
+              "target":"shell",
+              "client":"curl"
+            },{
+              "prismLanguage":"ruby",
+              "target":"ruby"
+            }]
+        }
   });
 
   //</editor-fold>
